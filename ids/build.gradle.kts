bf27ae8034179b43f090c9f82efb540018eb023f
Antonella Bonelli <antonella.bonelli@svelto.tech>
2025-05-31 18:12:18 +0200

    configurazione modulo simulator

--- ids/build.gradle.kts
+++ ids/build.gradle.kts
@@ -10,17 +10,17 @@ repositories {
 }
 
 dependencies {
-    implementation('com.google.inject:guice:7.0.0')
+    implementation("com.google.inject:guice:7.0.0")
 
     // Logging
-    implementation('org.slf4j:slf4j-api:2.0.6')
-    implementation('ch.qos.logback:logback-classic:1.4.5')
+    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
 
     // JUnit per i test
-    testImplementation('org.junit.jupiter:junit-jupiter-api:5.9.2')
-    testImplementation('org.junit.jupiter:junit-jupiter-engine:5.9.2')
-    testImplementation('org.mockito:mockito-core:5.1.1')
-    testImplementation('org.mockito:mockito-junit-jupiter:5.1.1')
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.mockito:mockito-core:5.1.1")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.1.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
